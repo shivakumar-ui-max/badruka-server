dafe8c2c353fce9b3856dc5c6c59a6e48971b371
shivakumar <skchintu000@gmail.com>
2024-05-27 00:52:29 +0530

    add client link

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // CORS configuration
 const options = {
-   origin: "http://localhost:5173",
+   origin: ["http://localhost:5173", "https://badruka-client.onrender.com"],
    credentials: true,
    methods: ["GET", "POST", "PUT", "DELETE"],
 };
